ee0f461b4a487a4382ea33bd33224320f1b332d0
panduramadhan2 <panduramadhan2@gmail.com>
2024-10-24 18:34:35 +0700

    Tugas 1 Mobile Dev React Native Expo

--- App.js
+++ App.js
@@ -1,20 +1,139 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Image,
+  Linking,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useEffect } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { kontak } from "./components/DataTugas1";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [loaded, error] = useFonts({
+    "Inter-Bold": require("./assets/fonts/Inter_18pt-Bold.ttf"),
+    "Inter-Regular": require("./assets/fonts/Inter_18pt-Regular.ttf"),
+    "Inter-Light": require("./assets/fonts/Inter_18pt-Light.ttf"),
+    "Inter-Medium": require("./assets/fonts/Inter_18pt-Medium.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+      <StatusBar barStyle={"light-content"} />
+      <Image
+        style={styles.profile}
+        source={require("./assets/tugas1/afif 1.png")}
+      />
+      <View style={styles.title}>
+        <Text style={styles.namDepan}>
+          Muhammad <Text style={styles.namaBelakang}>Afiffudin</Text>
+        </Text>
+        <Text style={styles.deskripsi}>Mobile Developer</Text>
+      </View>
+      <Text
+        style={{
+          fontFamily: "Inter-Regular",
+          fontSize: 20,
+          marginTop: 20,
+          marginBottom: 20,
+        }}
+      >
+        Portofolio
+      </Text>
+      <View style={styles.portofolio}>
+        <Image
+          style={styles}
+          source={require("./assets/tugas1/iPhone 16 Pro - 1 1.png")}
+        />
+        <Image
+          style={styles}
+          source={require("./assets/tugas1/iPhone 16 Pro - 1 1.png")}
+        />
+      </View>
+      <Text
+        style={{
+          fontFamily: "Inter-Regular",
+          fontSize: 20,
+          marginTop: 20,
+          marginBottom: 16,
+        }}
+      >
+        Kontak Saya
+      </Text>
+      {kontak.map((item) => (
+        <TouchableOpacity
+          key={item.id}
+          style={styles.item}
+          onPress={() => Linking.openURL(item.url)}
+        >
+          <Image source={item.image} style={styles.icon} />
+          <Text style={styles.text}>{item.nama}</Text>
+        </TouchableOpacity>
+      ))}
+      <View style={{ height: 100 }}></View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container: { padding: 29 },
+  profile: {
+    width: 152,
+    height: 152,
+    marginTop: 56,
+    alignSelf: "center",
+    borderRadius: 152 / 2,
+  },
+  title: {
+    marginTop: 29,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  namDepan: { fontFamily: "Inter-Regular", fontSize: 24 },
+  namaBelakang: { fontFamily: "Inter-Bold", fontSize: 24 },
+  deskripsi: {
+    fontFamily: "Inter-Regular",
+    fontSize: 14,
+  },
+  portofolio: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignContent: "center",
+    alignItems: "center",
+  },
+  item: {
+    borderWidth: 1,
+    borderColor: "#000000",
+    borderRadius: 10,
+    padding: 15,
+    marginBottom: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    fontFamily: "Inter-Regular",
+    fontSize: 16,
+  },
+  text: {
+    fontFamily: "Inter-Regular",
+    fontSize: 16,
+  },
+  icon: {
+    width: 34,
+    height: 34,
+    marginRight: 13,
   },
 });
---
+++ components/DataTugas1.js
@@ -0,0 +1,26 @@
+export const kontak = [
+  {
+    id: 1,
+    nama: "Instagram / @afifbasya",
+    url: "https://www.instagram.com/pandu.ra/",
+    image: require("../assets/tugas1/mdi_instagram.png"),
+  },
+  {
+    id: 2,
+    nama: "Website / afifbasya.com",
+    url: "https://www.instagram.com/pandu.ra/",
+    image: require("../assets/tugas1/mdi_web.png"),
+  },
+  {
+    id: 3,
+    nama: "Facebook / Afifuddin Basyaeban",
+    url: "https://www.instagram.com/pandu.ra/",
+    image: require("../assets/tugas1/ant-design_facebook-filled.png"),
+  },
+  {
+    id: 4,
+    nama: "Email / afifbasya@gmail.com",
+    url: "https://www.instagram.com/pandu.ra/",
+    image: require("../assets/tugas1/ic_baseline-email.png"),
+  },
+];
